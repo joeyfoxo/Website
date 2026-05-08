a848ccf0487321679f1ea974296416d8f3e431f1
Joey <james.lawrence@live.co.uk>
2026-05-08 21:45:36 +0100

    Remove old backend

--- backend/internal/models/models.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Credentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email,omitempty"` // optional on login
-}
